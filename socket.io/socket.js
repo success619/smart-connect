b13e5beacb09bef9f30dc3384831ead111145b62
success619 <aguogansuccess@gmail.com>
2024-09-13 11:52:32 +0100

    uploading some update to the unfinished projects, still working on it

--- socket.io/socket.js
+++ socket.io/socket.js
@@ -9,11 +9,13 @@ let users = []
 const addUser = (userId, socketId) => {
     !users.some((user) => user.userId === userId) &&
         users.push({ userId, socketId })
+    console.log(users)
 }
 
 
 const removeUser = (socketId) => {
     users = users.filter(user => user.socketId !== socketId)
+    console.log(users)
 }
 
 
@@ -21,6 +23,9 @@ const getUser = (recieverId) => {
     return users.find(user => user.userId === recieverId)
 }
 
+const getUserAllConnects = (allConnects) => {
+    return users.filter(f => allConnects.some(u => u.userId === f.userId))
+}
 
 io.on("connection", (socket) => {
     console.log("a user connected")
@@ -32,7 +37,7 @@ io.on("connection", (socket) => {
         if (userId !== null && userId !== undefined) {
             addUser(userId, socket.id);
             io.emit("getActiveUsers", users);
-            console.log("the user id = ", userId)
+            // console.log("the user id = ", userId)
         }
     })
 
@@ -40,13 +45,32 @@ io.on("connection", (socket) => {
     // send and get instant messages
     socket.on("sendMessage", ({ senderId, text, recieverId }) => {
         const user = getUser(recieverId);
-        io.to(user.socketId).emit("getMessage", {
-            senderId,
-            text
-        })
+        if (user?.socketId) {
+            io.to(user.socketId).emit("getMessage", {
+                senderId,
+                text
+            })
+        }
     })
 
 
+    // send and get instant notifications
+    socket.on("sendPostNotficationToAllConnects", ([postNotification, allConnect]) => {
+        const allActiveConnects = getUserAllConnects(allConnect);
+        for (let index = 0; index < allActiveConnects.length; index++) {
+            const user = allActiveConnects[index];
+            io.to(user.socketId).emit("getInstantNotifications", postNotification)
+            console.log(postNotification)
+        }
+    })
+
+    socket.on("postFollowerNotificationToUserIfActive", (req) => {
+        const user = getUser(req.user_id)
+        if (user.socketId) {
+            io.to(user.socketId).emit("getInstantFollowerNotification", req)
+        }
+        console.log(user, req)
+    })
 
 
     // user disconnected or go offline
